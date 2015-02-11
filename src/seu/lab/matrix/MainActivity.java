25fd141c59b4b4bb4a4b783f93ea0fe26fdaf0dd
yqf3139 <yqf3139@gmail.com>
2015-02-11 12:01:56 +0800

    gson & volley tested

--- src/seu/lab/matrix/MainActivity.java
+++ src/seu/lab/matrix/MainActivity.java
@@ -1,25 +1,60 @@
 package seu.lab.matrix;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.android.volley.Request.Method;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 import com.idisplay.VirtualScreenDisplay.ConnectionActivity;
 import com.idisplay.VirtualScreenDisplay.ConnectionActivity.ConnectionType;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
+	public static final String TAG = "MainActivity";
+	
+	private RequestQueue mQueue = null;
+
+	private Gson gson = new Gson();
+	
+	private ImageView imageView;
+	
+	class Folder{
+		public String res = "";
+		public String name;
+		public String location;
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_main);
 		
 		Button single = (Button) findViewById(R.id.connect_btn_single);
 		Button duel = (Button) findViewById(R.id.connect_btn_duel);
 		Button cardboardButton = (Button) findViewById(R.id.cardboard_btn);
+		Button connetButton = (Button) findViewById(R.id.connect_btn);
+		imageView = (ImageView) findViewById(R.id.image_view);
 		
 		single.setOnClickListener(new OnClickListener() {
 			
@@ -52,5 +87,63 @@ public class MainActivity extends Activity {
 				finish();
 			}
 		});
+		
+		connetButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				try {
+					testVolley();
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		mQueue = Volley.newRequestQueue(getApplicationContext());
+	}
+	
+	public void testVolley() throws JSONException {
+		
+		Folder folder = new Folder();
+		folder.name = "ff1";
+		folder.location = "ll1";
+		
+		String url = "http://192.168.1.102:50305/api/test";
+        Log.d(TAG, "req : " + url);
+
+        ErrorListener errorListener = new ErrorListener() {
+
+			@Override
+			public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "onError : " + error.toString());
+			}
+		};
+		
+		Listener<JSONObject> listener = new Listener<JSONObject>() {
+
+			@Override
+			public void onResponse(JSONObject res) {
+				Folder folder = gson.fromJson(res.toString(), Folder.class);
+				
+                Log.d(TAG, "onResponse : " + folder.res);
+			}
+		};
+		
+		Listener<Bitmap> imgListener = new Listener<Bitmap>() {
+
+			@Override
+			public void onResponse(Bitmap bitmap) {
+                Log.d(TAG, "onResponse : " + bitmap.getWidth() + "x" + bitmap.getHeight());
+                imageView.setImageBitmap(bitmap);
+			}
+		};
+
+		//mQueue.add(new JsonObjectRequest(Method.POST, url, new JSONObject(gson.toJson(folder)), listener, errorListener));
+		
+		mQueue.add(new ImageRequest(url, imgListener, 2000, 2000, Config.ARGB_8888, errorListener));
+		
+		mQueue.start();
 	}
 }
